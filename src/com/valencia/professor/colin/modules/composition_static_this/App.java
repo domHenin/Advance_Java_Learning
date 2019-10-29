2f38f46a7f6502070421d993a415bd19b25beb0c
domHenin <domhenin.517@gmail.com>
2019-10-29 16:08:08 -0400

    still working on video @ 18:11

---
+++ src/com/valencia/professor/colin/modules/composition_static_this/App.java
@@ -0,0 +1,9 @@
+package com.valencia.professor.colin.modules.composition_static_this;
+
+public class App {
+    public static void main(String[] args) {
+        int maxSpeed = RaceCar.getMaxSpeedForAll();
+
+        RaceCar andretti = new RaceCar()
+    }
+}
